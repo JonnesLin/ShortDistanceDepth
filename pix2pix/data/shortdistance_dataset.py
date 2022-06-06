50c4f796a2d462bc434e01226fe83edf24809309
Jones Lin <jlin398@wisc.edu>
2022-06-05 21:49:34 -0500

    Updated data transform.

--- pix2pix/data/shortdistance_dataset.py
+++ pix2pix/data/shortdistance_dataset.py
@@ -1,7 +1,7 @@
 from pix2pix.data.base_dataset import BaseDataset
 from pix2pix.data.image_folder import make_dataset
 from pix2pix.util.guidedfilter import GuidedFilter
-
+from torchvision import transforms
 import numpy as np
 import os
 import torch
@@ -36,23 +36,27 @@ class ShortDistanceDataset(BaseDataset):
         self.depth_map_paths = os.listdir(self.dir_depth_map)
 
         self.transform = transform
+        self.depthData_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor()
+        ])
 
     def __getitem__(self, index):
         # Read data
         rgb_path = os.path.join(self.dir_rgb, self.rgb_paths[index])
-        depth_map_path = os.path.join(self.dir_depth_map, self.depth_map_paths[index])
+        depth_map_path = os.path.join(self.dir_depth_map, self.rgb_paths[index].replace("jpg", "png"))
         data_rgb = read_image_data(rgb_path)  # needs to be a tensor
         data_depth_map = read_image_data(depth_map_path)  # needs to be a tensor
+        # Do depth map transform
+        data_depth_map = self.depthData_transform(data_depth_map)
 
         # Only take one channel for the depth map
-        data_depth_map = torch.tensor(data_depth_map).float()
         data_depth_map = data_depth_map.permute((1, 2, 0)).numpy()[:, :, 0:1]
 
-        # Transform
-        if self.transform is not None:
-            combined_images = self.transform(image=data_rgb, mask=data_depth_map)
-            data_rgb, data_depth_map = combined_images['image'], combined_images['mask']
-            data_depth_map = data_depth_map.squeeze(0)
+        # Data Transform
+        combined_images = self.transform(image=data_rgb, mask=data_depth_map)
+        data_rgb, data_depth_map = combined_images['image'], combined_images['mask']
+        data_depth_map = data_depth_map.squeeze(0)
 
         return data_rgb, data_depth_map
 
